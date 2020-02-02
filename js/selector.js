80d7c6b2d5f9346b35c12b38296d51ed7887c8a4
quimnuss <lluna.nova@gmail.com>
2020-02-02 18:35:09 +0100

    working selector

---
+++ js/selector.js
@@ -0,0 +1,9 @@
+$(document).ready(function ($) {
+  $("#accordeon dl:not(:first-child)").hide();
+
+  $("#thumb-selector a").click(function(e){
+    e.preventDefault();
+    $('#pnl-'+this.name).show('fast');
+    $('#pnl-'+this.name).siblings().hide();
+  });
+})
